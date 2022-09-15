50ae644c5da864b9bd680951eb13d3cac33798c9
leonardofmds <leonardofelipe.nave@gmail.com>
2022-09-14 21:32:02 -0300

    adição do projeto react

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,8 @@ app.get('/ads', (request, response) => {
     return response.json([
         {id: 1, nome: 'Anúncio 1'},
         {id: 2, nome: 'Anúncio 2'},
-        {id: 3, nome: 'Anúncio 3'}
+        {id: 3, nome: 'Anúncio 3'},
+        {id: 3, nome: 'Anúncio 4'}
     ])
 })
 
